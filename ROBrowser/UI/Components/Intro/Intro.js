e40e5cc9220816a1b4e533372e87548297b43cdf
Vincent Thibault <vthibault.mobile@gmail.com>
2013-11-22 16:02:07 +0100

    Introduce new Intro Window with user configuration ( screen size, full screen, server list, … )

--- ROBrowser/UI/Components/Intro/Intro.js
+++ ROBrowser/UI/Components/Intro/Intro.js
@@ -16,11 +16,14 @@ define(function(require)
 	 * Dependencies
 	 */
 	var jQuery      = require('Utils/jquery');
+	var KEYS        = require('Controls/KeyEventHandler');
 	var UIManager   = require('UI/UIManager');
 	var UIComponent = require('UI/UIComponent');
-	var Metaling    = require('./Metaling');
 	var htmlText    = require('text!./Intro.html');
 	var cssText     = require('text!./Intro.css');
+	var Particle    = require('./Particle');
+	var Preferences = require('Core/Preferences');
+	var Context     = require('Core/Context');
 
 
 	/**
@@ -30,60 +33,330 @@ define(function(require)
 
 
 	/**
+	 * Files to load
+	 */
+	Intro.files = [];
+
+
+	/**
+	 * Intro preferences
+	 */
+	Intro.preferences = Preferences.get('Window', {
+		screensize:  '800x600',
+		quality:      100,
+		serverfile:  'clientinfo.xml',
+		serverlist:  [],
+		serverdef:   'serverlist'
+	}, 1.0 );
+
+
+	/**
+	 * Manage Escape key to exit
+	 */
+	Intro.onKeyDown = function OnKeyDown( event )
+	{
+		// Exit fullScreen mode
+		if( event.which === KEYS.ESCAPE ) {
+
+			if( Context.isFullScreen() ) {
+				Context.cancelFullScreen();
+			}
+
+			event.stopImmediatePropagation();
+			return false;
+		}
+
+		return true;
+	};
+
+
+	/**
 	 * Initialize Metaling
 	 */
 	Intro.init = function Init()
 	{
-		Metaling.init( this.ui.find('canvas')[0], this.onInit.bind(this));
-	};
+		var ui = this.ui;
 
+		this.preloadImages();
 
-	/**
-	 * Initialize box
-	 *
-	 */
-	Intro.onInit = function OnInit()
-	{
-		// Bind Input
-		this.ui.find('button').one('click', this.process );
-		this.ui.find('input[type="file"]').on('change', this.process);
-	
-		// Bind canvas
-		this.ui.find('canvas')
-			.click(function(){
-				Intro.ui.find('input[type="file"]').click();
+		// About page
+		ui.find('.btn_about')
+			.mousedown(function(){
+				var $about =  ui.find('.overlay.about');
+				$about
+					.show()
+					.animate({opacity:1}, 200)
+					.click(function(){
+						$about.animate({opacity:0}, 200, function(){
+							$about.hide();
+						})
+					});
+			});
+
+		// Settings page
+		ui.find('.btn_settings')
+			.mousedown(function(){
+				ui.find('.overlay.settings')
+					.show()
+					.animate({opacity:1}, 200);
+			});
+
+		// Box to set files
+		ui.find('.box')
+			.mouseover(function(){
+				jQuery(this).addClass('over');
 			})
+			.mouseout(function(){
+				jQuery(this).removeClass('over');
+			})
+			.click(function(){
+				ui.find('input[type="file"]').click();
+			})
+			.on('drop', this.process )
 			.on('dragover', function(){
-				Metaling.setType(Metaling.TYPE.LOVE);
-				Intro.ui.find('article').text('');
+				jQuery(this).addClass('over');
 				return false;
 			})
 			.on('dragleave', function(){
-				Metaling.setType(Metaling.TYPE.SAD);
-				Intro.ui.find('article').text('');
+				jQuery(this).removeClass('over');
 				return false;
 			})
-			.on('drop', this.process );
-	
-		// Stop action on window
-		jQuery(window)
-			.on('dragover, dragleave', function( event ){
+
+		// Input handler
+		ui.find('input[type="file"]')
+			.on('change', this.process );
+
+
+
+		// Modify quality
+		ui.find('.quality')
+			.on('change', function(){
+				ui.find('.quality_result').text( this.value + '%' );
+			});
+
+		// Stop propagation in overlay to avoid hiding the page
+		ui.find('.overlay')
+			.on('click', 'input[type="text"], a, button', function( event ){
+				if( this.nodeName === 'INPUT' ) {
+					this.select();
+				}
 				event.stopImmediatePropagation();
 				return false;
-			})
-			.on('drop', this.process );
+			});
+
+		// Add Server
+		ui.find('.btn_add')
+			.on('click', function(){
+				var $serverlist = ui.find('.servers');
+				var count = $serverlist.find('tr').length;
+				$serverlist.append(
+					'<tr>' +
+					'	<td><input type="text" class="display" value="Server '+ count +'"/></td>' +
+					'	<td><input type="text" class="address" value="127.0.0.1:6900"/></td>' +
+					'	<td><input type="text" class="version"value="22"/></td>' +
+					'	<td><input type="text" class="langtype" value="12"/></td>' +
+					'	<td><input type="text" class="packetver"value="auto"/></td>' +
+					'	<td><button class="btn_delete"></button></td>' +
+					'</tr>'
+				);
+
+				$serverlist.find('tr :eq('+ count +') input:first').focus();
+			});
+
+		ui.find('.btn_save')
+			.on('click', function(){
+				Intro.savePreferences();
+				ui.find('.overlay')
+					.animate({opacity:0}, 200, function(){
+						ui.find('.overlay').hide();
+					})
+			});
+
+		ui.find('.serverlist tbody')
+			.on('click', '.btn_delete', function(){
+				jQuery(this).parent().parent().remove();
+			});
+
+		// Start roBrowser
+		ui.find('.btn_play')
+			.click(function(){
+				ui.find('.overlay.loading')
+					.show()
+					.animate({opacity:1}, 200);
+
+				Intro.onFilesSubmit( Intro.files );
+			});
 	};
 
 
 	/**
-	 * Once in HTML, focus the input
+	 * Resize window
 	 */
 	Intro.onAppend = function OnAppend()
 	{
-		this.ui.animate({ opacity: 1.0 },1000);
-		this.ui.find('header').css({ top:-100, opacity:0 }).animate({ top:    0, opacity:1 }, 1000);
-		this.ui.find('footer').css({ left:  0, opacity:0 }).animate({ left: 200, opacity:1 }, 1000);
-		this.ui.find('article').css({ opacity:0 }).animate({ opacity:1 }, 2000);
+		var $window = jQuery(window);
+		var $intro  = this.ui.find('.intro');
+
+		$window.on('resize.intro',function(){
+			$intro.css(
+				'transform',
+				'scale('+
+					$window.width()  / $intro.width()  +
+					',' +
+					$window.height() / $intro.height() +
+				')'
+			);
+		});
+
+		$window.trigger('resize.intro');
+
+		Particle.init( 100, this.ui.find('canvas')[0] );
+
+		this.loadPreferences();
+	};
+
+
+	/**
+	 * Remove auto-resize
+	 */
+	Intro.onRemove = function OnRemove()
+	{
+		jQuery(window).off('resize.intro');
+		Particle.stop();
+		this.ui.find('.overlay.loading').hide();
+	}
+
+
+	/**
+	 * Load images to add some styles to the Intro
+	 */
+	Intro.preloadImages = function PreloadImages()
+	{
+		// Background images
+		jQuery('style:first').append([
+			'#intro .intro { background-image:url(' + require.toUrl('./images/background.jpg') + '); }',
+			'#intro .ribbon { background-image:url(' + require.toUrl('./images/ribbon.png') + '); }',
+			'#intro .box { background-image:url(' + require.toUrl('./images/box.jpg') + '); }',
+			'#intro .btn_play { background-image:url(' + require.toUrl('./images/play.png') + '); }',
+			'#intro .btn_play:hover { background-image:url(' + require.toUrl('./images/play-down.png') + '); }',
+			'#intro .btn_add { background-image:url('+ require.toUrl('./images/add-server.jpg') +'); }',
+			'#intro .btn_save { background-image:url('+ require.toUrl('./images/save.jpg') + '); }',
+			'#intro .btn_delete { background-image:url('+ require.toUrl('./images/delete.png') + '); }'
+		].join('\n'));
+
+		// Add images to IMG tag
+		this.ui.find('.icon img').attr('src', require.toUrl('./images/icon.png') );
+		this.ui.find('.btn_about img').attr('src', require.toUrl('./images/about.png') );
+		this.ui.find('.btn_settings img').attr('src', require.toUrl('./images/settings.png') );
+
+		// Preload image
+		(new Image).src = require.toUrl('./images/play-down.png');
+	};
+
+
+	/**
+	 * Bind html with preferences
+	 */
+	Intro.loadPreferences = function LoadPreferences()
+	{
+		this.ui.find('.screensize').val( this.preferences.screensize );
+		this.ui.find('.quality').val( this.preferences.quality ).trigger('change');
+
+		this.ui.find('.serverdef').attr('checked', false );
+		this.ui.find('.serverdef[value="'+ this.preferences.serverdef +'"]').attr('checked', 'checked');
+		this.ui.find('.clientinfo').val( this.preferences.serverfile );
+
+		var i, count;
+		var serverlist = this.preferences.serverlist;
+		var $servers = this.ui.find('.servers').empty();
+
+		for( i = 0, count = serverlist.length; i < count; ++i ) {
+			$servers.append(
+				'<tr>' +
+				'	<td><input type="text" class="display" value="'+ serverlist[i].display +'"/></td>' +
+				'	<td><input type="text" class="address" value="'+ serverlist[i].address +':'+ serverlist[i].port +'"/></td>' +
+				'	<td><input type="text" class="version" value="'+ serverlist[i].version +'"/></td>' +
+				'	<td><input type="text" class="langtype" value="'+ serverlist[i].langtype +'"/></td>' +
+				'	<td><input type="text" class="packetver" value="'+ serverlist[i].packetver + '"/></td>' +
+				'	<td><button class="btn_delete"></button></td>' +
+				'</tr>'
+			);
+		}
+
+		this.applyPreferences();
+	};
+
+
+	/**
+	 * Save preferences to user storage.
+	 */
+	Intro.savePreferences = function SavePreferences()
+	{
+		this.preferences.screensize = this.ui.find('.screensize').val();
+		this.preferences.quality    = this.ui.find('.quality').val();
+		this.preferences.serverdef  = this.ui.find('.serverdef:checked').val();
+		this.preferences.serverfile = this.ui.find('.clientinfo').val();
+		this.preferences.serverlist = [];
+
+		var $servers = this.ui.find('.servers');
+		var i, count = $servers.find('tr').length;
+		var $server;
+
+		for( i = 0; i < count; ++i ) {
+			$server = $servers.find('tr:eq('+ i +')');
+			this.preferences.serverlist.push({
+				display:   $server.find('.display').val(),
+				address:   $server.find('.address').val().split(':')[0],
+				port:      parseInt( $server.find('.address').val().split(':')[1], 10),
+				version:   $server.find('.version').val(),
+				langtype:  $server.find('.langtype').val(),
+				packetver: $server.find('.packetver').val()
+			});
+		}
+
+		this.preferences.save();
+		this.applyPreferences();
+	};
+
+
+	/**
+	 * Apply preferences, resize window, etc
+	 */
+	Intro.applyPreferences = function ApplyPreferences()
+	{
+		window.ROConfig = window.ROConfig || {};
+
+		var isFullScreen = Context.isFullScreen();
+
+		// Full Screen support
+		if( this.preferences.screensize === "full" ) {
+			if(!isFullScreen ) {
+				Context.requestFullScreen();
+			}
+		}
+		else {
+	
+			if( isFullScreen ) {
+				Context.cancelFullScreen();
+			}
+
+			// Resizing
+			if( Context.Is.POPUP ) {
+				var size = this.preferences.screensize.split("x");
+				window.resizeTo( size[0], size[1] );
+				window.moveTo( (screen.availWidth - size[0]) / 2, (screen.availHeight - size[1]) / 2 );
+			}
+		}
+
+		// Bind data
+		if( this.preferences.serverdef === 'serverlist' ) {
+			ROConfig.servers = this.preferences.serverlist;
+		}
+		else {
+			ROConfig.servers = 'data/' + this.preferences.serverfile;
+		}
+
+		ROConfig.quality = this.preferences.quality;
 	};
 
 
@@ -94,12 +367,14 @@ define(function(require)
 	 */
 	Intro.process = function Process( event )
 	{
-		Metaling.setType(Metaling.TYPE.LOAD);
-		Intro.ui.find('button').hide();
-		Intro.ui.find('article').text('Loading, please wait...');
+		jQuery(this).removeClass('over');
 
-		// Initialize the client
-		Intro.onFilesSubmit(this.files || ( event.originalEvent.dataTransfer && event.originalEvent.dataTransfer.files ) || []);
+		var files = this.files || ( event.originalEvent.dataTransfer && event.originalEvent.dataTransfer.files ) || [];
+
+		if( files.length ) {
+			Intro.files.push.apply( Intro.files, files );
+			Intro.ui.find('.msg').text( Intro.files.length + ' files selected' );
+		}
 
 		event.stopImmediatePropagation();
 		return false;
@@ -109,7 +384,7 @@ define(function(require)
 	/**
 	 * Callback to used.
 	 */
-	Intro.onFilesSubmit = function OnFilesSubmit(){};
+	Intro.onFilesSubmit = function OnFilesSubmit( files ){};
 
 
 	/**
---
+++ ROBrowser/UI/Components/Intro/Particle.js
@@ -0,0 +1,185 @@
+/**
+ * UI/Components/Intro/Particles.js
+ *
+ * Manage Intro Particles
+ *
+ * This file is part of ROBrowser, Ragnarok Online in the Web Browser (http://www.robrowser.com/).
+ *
+ * @author Vincent Thibault
+ */
+
+define(function( require )
+{
+	"use strict";
+
+	/**
+	 * @Constructor
+	 */
+	function Particle( width, height ) {
+
+		this.rmax = 10;
+		this.xmax = 5;
+		this.ymax = 2;
+
+		this.x    = width  * Math.random();
+		this.y    = height * Math.random();
+		this.r    = (this.rmax-1) * Math.random() + 1;
+
+		this.c    = Math.floor( 255 * Math.random() );
+
+		this.dx   = Math.random() * this.xmax * 2 - this.xmax;
+		this.dy   = Math.random() * this.ymax * 2 - this.ymax;
+		this.dc   = Math.random() > 0.5 ? 1 : -1;
+
+		this.life  = 100 * ( this.r/ this.rmax );
+		this.ratio = Math.random() * this.life;
+
+		this.stop = Math.random() * .2 +.4;
+		this.t    = Math.random() + 1;
+	}
+
+
+	/**
+	 * Cross Browser animation Frame
+	 */
+	Particle.requestRender = window.requestAnimationFrame
+		|| window.webkitRequestAnimationFrame
+		|| window.mozRequestAnimationFrame
+		|| window.oRequestAnimationFrame
+		|| window.msRequestAnimationFrame
+		|| function(callback, element){
+			window.setTimeout( callback, 1000/60 );
+		};
+
+
+	/**
+	 * Initialize particles
+	 *
+	 * @param {number} particles count
+	 */
+	Particle.init = function Init( count, canvas )
+	{
+		this.list   = new Array(count);
+
+		this.width  = canvas.width;
+		this.height = canvas.height;
+		this.canvas = canvas;
+		this.ctx    = canvas.getContext('2d');
+		this.ctx.globalCompositeOperation = "lighter";
+
+		this.bg     = new Image();
+		this.bg.src = require.toUrl('./images/background.jpg');
+
+		for( var i = 0; i < count; ++i ) {
+			this.list[i] = new Particle( this.width, this.height );
+		}
+
+		this.process = true;
+		this.tick    =    0;
+		this.render();
+	};
+
+
+	/**
+	 * Stop particles
+	 */
+	Particle.stop = function Stop()
+	{
+		this.process = false;
+	};
+
+
+	/**
+	 * Render particles
+	 */
+	Particle.render = function Render()
+	{
+		var now = (new Date).getTime();
+		var i, count;
+
+		if( now - this.tick > 50 ) {
+			this.ctx.clearRect( 0, 0, this.width, this.height );
+			if( this.bg.complete )
+				this.ctx.drawImage(this.bg, 0, 0);
+
+			for( i = 0, count = this.list.length; i < count; ++i ) {
+				this.list[i].move( 0, 0, this.width, this.height );
+				this.list[i].render( this.ctx );
+			}
+
+			this.tick = now;
+		}
+
+		if( this.process ) {
+			this.requestRender.call(
+				window,
+				this.render.bind(this),
+				this.canvas
+			);
+		}
+	};
+
+
+	/**
+	 * Render particle
+	 *
+	 * @param {Canvas2DContext}
+	 */
+	Particle.prototype.render = function Render( ctx )
+	{
+		this.ratio += this.t;
+		if( this.ratio <= 0 || this.ratio >= this.life ) {
+			this.t *= -1;
+		}
+
+		var opacity  = (1 - ( this.ratio / this.life )).toFixed(2);
+		var radius   = this.r * opacity;
+		var gradient = ctx.createRadialGradient(
+			this.x | 0, this.y | 0,  0,
+			this.x | 0, this.y | 0, (radius <= 0 ? 1 : radius | 0)
+		);
+
+		ctx.beginPath();
+		ctx.arc( this.x | 0, this.y | 0, this.r | 0, 0, Math.PI * 2, true);
+		ctx.closePath();
+
+		gradient.addColorStop(0.0, 'rgba(255,255,255,' + opacity + ')');
+		gradient.addColorStop(this.stop, 'rgba('+ this.c +',100,255,' + (opacity*.6) + ')');
+		gradient.addColorStop(1.0, 'rgba('+ this.c +',100,255,0)');
+
+		ctx.fillStyle = gradient;
+		ctx.fill();
+	};
+
+
+	/**
+	 * Move particle in a rectangle
+	 *
+	 * @param {number} x
+	 * @param {number} y
+	 * @param {number} width
+	 * @param {number} height
+	 */
+	Particle.prototype.move = function Move( x, y, width, height )
+	{
+		this.x += (this.ratio/this.life) * this.dx;
+		this.y += (this.ratio/this.life) * this.dy;
+		this.c += this.dc;
+
+		// Bound
+		if( this.x >= width )  this.dx = -1;
+		else if( this.x <= x ) this.dx =  1;
+
+		if( this.y >= height ) this.dy = -1;
+		else if( this.y <= y ) this.dy =  1;
+
+		if( this.c >= 255 )    this.dc = -1;
+		else if( this.c <= 0 ) this.dc =  1;
+	};
+
+
+	/**
+	 * Export class
+	 */
+	return Particle;
+});
