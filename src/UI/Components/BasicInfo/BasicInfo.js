7147d84bf1b2ada56c02c8ac69d6c35cbbb7335e
Vincent Thibault <vthibault.mobile@gmail.com>
2014-03-17 10:03:28 +0100

    Avoid getting 2365571% exp in BasicInfo, cap the value to 100%

--- src/UI/Components/BasicInfo/BasicInfo.js
+++ src/UI/Components/BasicInfo/BasicInfo.js
@@ -273,7 +273,7 @@ define(function(require)
 
 				this.ui.find('.'+ type).show();
 				this.ui.find('.'+ type +' div').css('width', Math.min( 100, Math.floor(val1 * 100 / val2) ) + '%');
-				this.ui.find('.'+ type +'_value').text( (Math.floor(val1 * 1000 / val2) * 0.1).toFixed(1) + '%');
+				this.ui.find('.'+ type +'_value').text( Math.min( 100, (Math.floor(val1 * 1000 / val2) * 0.1).toFixed(1)) + '%');
 				break;
 
 			case 'weight':
