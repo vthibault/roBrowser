8d964f3995ef69a6de0d720c2279d112431e56a8
herenow <leonardoshiro@gmail.com>
2014-05-05 18:36:42 -0300

    added listen to host support

--- utils/webserver.go
+++ utils/webserver.go
@@ -6,19 +6,25 @@ import "flag"
 import "log"
 
 var port int
+var host string
 var path string
 
 func init() {
+	 const defaultPort = 8080
+	 const defualtHost = "0.0.0.0"
+	 const defaultDir  = "."
+
          flag.IntVar(&port, "p", 8080, "Port that this webserver should listen to. Default: 8080")
+	 flag.StringVar(&host, "l", "0.0.0.0", "Default host for this webserver to listen to. Default: 0.0.0.0")
          flag.StringVar(&path, "d", "./", "The path to the directory this webserver should serve. Default: spawned directory")
 }
 
 func main() {
 	flag.Parse()
 
-	fmt.Printf("Starting webserver on port %d and serving path %s\n", port, path)
+	fmt.Printf("Starting webserver on  %s:%d and serving path %s\n", host, port, path)
 
-	listen := fmt.Sprintf(":%d", port)
+	listen := fmt.Sprintf("%s:%d", host, port)
 
 	log.Fatal(http.ListenAndServe(listen, Log(http.FileServer(http.Dir(path)))))
 }
