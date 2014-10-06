3a45355c08fbd8ace7bad75e84bc880401f27a34
Vincent Thibault <vthibault.mobile@gmail.com>
2014-10-06 09:24:18 +0200

    Following #670ec118 add skipped files

---
+++ applications/settings.js
@@ -0,0 +1,5 @@
+// Your custom settings
+// See parameters in http://www.robrowser.com/getting-started#API
+var ROConfig = {
+	development: true, // don't need to compile javascript files in chrome app since it's already a package.
+};
