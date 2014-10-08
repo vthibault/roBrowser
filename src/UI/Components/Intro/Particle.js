a93d7e16aefe0ddbfb73550a53658f848bb89e91
Vincent Thibault <vthibault.mobile@gmail.com>
2014-10-08 10:24:34 +0200

    Fix lag impression on intro particles

--- src/UI/Components/Intro/Particle.js
+++ src/UI/Components/Intro/Particle.js
@@ -21,21 +21,7 @@ define(function( require )
 		this.xmax = 5;
 		this.ymax = 2;
 
-		this.x    = width  * Math.random();
-		this.y    = height * Math.random();
-		this.r    = (this.rmax-1) * Math.random() + 1;
-
-		this.c    = Math.floor( 255 * Math.random() );
-
-		this.dx   = Math.random() * this.xmax * 2 - this.xmax;
-		this.dy   = Math.random() * this.ymax * 2 - this.ymax;
-		this.dc   = Math.random() > 0.5 ? 1 : -1;
-
-		this.life  = 100 * ( this.r/ this.rmax );
-		this.ratio = Math.random() * this.life;
-
-		this.stop = Math.random() * 0.2 + 0.4;
-		this.t    = Math.random() + 1;
+		this.reset(width, height);
 	}
 
 
@@ -57,7 +43,7 @@ define(function( require )
 	 *
 	 * @param {number} particles count
 	 */
-	Particle.init = function Init( count, canvas )
+	Particle.init = function init( count, canvas )
 	{
 		this.list   = new Array(count);
 
@@ -76,7 +62,7 @@ define(function( require )
 		}
 
 		this.process = true;
-		this.tick    =    0;
+		this.tick    = Date.now();
 		this.render();
 	};
 
@@ -84,7 +70,7 @@ define(function( require )
 	/**
 	 * Stop particles
 	 */
-	Particle.stop = function Stop()
+	Particle.stop = function stop()
 	{
 		this.process = false;
 	};
@@ -93,24 +79,22 @@ define(function( require )
 	/**
 	 * Render particles
 	 */
-	Particle.render = function Render()
+	Particle.render = function render()
 	{
 		var now = Date.now();
 		var i, count;
 
-		if (now - this.tick > 50) {
-			this.ctx.clearRect( 0, 0, this.width, this.height );
-			if (this.bg.ready) {
-				this.ctx.drawImage(this.bg, 0, 0);
-			}
-
-			for (i = 0, count = this.list.length; i < count; ++i) {
-				this.list[i].move( 0, 0, this.width, this.height );
-				this.list[i].render( this.ctx );
-			}
-
-			this.tick = now;
+		this.ctx.clearRect( 0, 0, this.width, this.height );
+		if (this.bg.ready) {
+			this.ctx.drawImage(this.bg, 0, 0);
 		}
+
+		for (i = 0, count = this.list.length; i < count; ++i) {
+			this.list[i].update( now-this.tick, 0, 0, this.width, this.height);
+			this.list[i].render( this.ctx );
+		}
+
+		this.tick = now;
 
 		if (this.process) {
 			this.requestRender.call(
@@ -123,31 +107,52 @@ define(function( require )
 
 
 	/**
+	 * Reset particles informations
+	 *
+	 * @param {number} width
+	 * @param {number} height
+	 */
+	Particle.prototype.reset = function reset( width, height)
+	{
+		this.x    = width  * Math.random();
+		this.y    = height * Math.random();
+		this.r    = (this.rmax-1) * Math.random() + 1;
+
+		this.c    = Math.floor( 255 * Math.random() );
+
+		this.dx   = Math.random() * this.xmax * 2 - this.xmax;
+		this.dy   = Math.random() * this.ymax * 2 - this.ymax;
+		this.dc   = Math.random() > 0.5 ? 1 : -1;
+
+		this.life  = 200 * ( this.r/ this.rmax );
+		this.ratio = Math.random() * this.life;
+
+		this.stop = Math.random() * 0.2 + 0.4;
+		this.t    = Math.random() + 1;
+	};
+
+
+	/**
 	 * Render particle
 	 *
 	 * @param {Canvas2DContext}
 	 */
-	Particle.prototype.render = function Render( ctx )
+	Particle.prototype.render = function render( ctx )
 	{
-		this.ratio += this.t;
-		if (this.ratio <= 0 || this.ratio >= this.life) {
-			this.t *= -1;
-		}
-
 		var opacity  = (1 - ( this.ratio / this.life )).toFixed(2);
 		var radius   = this.r * opacity;
 		var gradient = ctx.createRadialGradient(
-			this.x | 0, this.y | 0,  0,
-			this.x | 0, this.y | 0, (radius <= 0 ? 1 : radius | 0)
+			this.x, this.y,  0,
+			this.x, this.y, (radius <= 0 ? 1 : radius | 0)
 		);
 
 		ctx.beginPath();
-		ctx.arc( this.x | 0, this.y | 0, this.r | 0, 0, Math.PI * 2, true);
+		ctx.arc( this.x, this.y, this.r, 0, Math.PI * 2, true);
 		ctx.closePath();
 
 		gradient.addColorStop(0.0, 'rgba(255,255,255,' + opacity + ')');
-		gradient.addColorStop(this.stop, 'rgba('+ this.c +',100,255,' + (opacity*0.6) + ')');
-		gradient.addColorStop(1.0, 'rgba('+ this.c +',100,255,0)');
+		gradient.addColorStop(this.stop, 'rgba('+ (this.c|0) +',100,255,' + (opacity*0.6) + ')');
+		gradient.addColorStop(1.0, 'rgba('+ (this.c|0) +',100,255,0)');
 
 		ctx.fillStyle = gradient;
 		ctx.fill();
@@ -157,23 +162,32 @@ define(function( require )
 	/**
 	 * Move particle in a rectangle
 	 *
+	 * @param {number} time elapsed
 	 * @param {number} x
 	 * @param {number} y
 	 * @param {number} width
 	 * @param {number} height
 	 */
-	Particle.prototype.move = function Move( x, y, width, height )
+	Particle.prototype.update = function move( tick, x, y, width, height )
 	{
-		this.x += (this.ratio/this.life) * this.dx;
-		this.y += (this.ratio/this.life) * this.dy;
-		this.c += this.dc;
+		var f = tick/60*2; // speed / 2
+
+		this.x += Math.cos((this.ratio/this.life) * this.dx * 2) * f;
+		this.y += Math.sin((this.ratio/this.life) * this.dy * 2) * f;
+		this.c += this.dc * f;
+
+		this.ratio += this.t * f;
+		if (this.ratio <= 0 || this.ratio >= this.life) {
+			this.t *= -1;
+		}
 
 		// Bound
-		if (this.x >= width)  this.dx = -1;
-		else if (this.x <= x) this.dx =  1;
-
-		if (this.y >= height) this.dy = -1;
-		else if (this.y <= y) this.dy =  1;
+		if (this.x - this.r > width ||
+		    this.x + this.r < 0 ||
+		    this.y - this.r > height ||
+		    this.y + this.r < 0) {
+			this.reset(width, height);
+		}
 
 		if (this.c >= 255)    this.dc = -1;
 		else if (this.c <= 0) this.dc =  1;
